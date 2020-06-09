28062b0717bb71622b3155bf7e12692be39233bd
root <root@localhost.localdomain>
2020-06-09 18:28:20 +0530

    KOGITO-2386: build config on common package

---
+++ packages/common/webpack.common.js
@@ -0,0 +1,159 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const nodeExternals = require("webpack-node-externals");
+const CircularDependencyPlugin = require("circular-dependency-plugin");
+const webpack = require('webpack');
+const BG_IMAGES_DIRNAME = 'bgimages';
+
+module.exports = {
+  entry: {
+    index: "./index.ts"
+  },
+  devtool: "inline-source-map",
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].js',
+    libraryTarget: "umd",
+    globalObject: "this"
+  },
+  plugins: [
+    // new HtmlWebpackPlugin({
+    //   template: path.resolve(__dirname, 'src', 'index.html'),
+    //   favicon: 'src/favicon.ico'
+    // }),
+    // new webpack.EnvironmentPlugin({
+    //   KOGITO_AUTH_ENABLED: false,
+    //   KOGITO_KEYCLOAK_REALM: 'kogito',
+    //   KOGITO_KEYCLOAK_URL: 'http://localhost:8280',
+    //   KOGITO_KEYCLOAK_CLIENT_ID: 'kogito-management-console',
+    //   KOGITO_DATAINDEX_HTTP_URL: 'http://localhost:4000/graphql',
+    //   KOGITO_APP_VERSION: 'DEV',
+    //   KOGITO_APP_NAME: 'Management Console'
+    // }),
+    new CircularDependencyPlugin({
+      exclude: /node_modules/, // exclude detection of files based on a RegExp
+      failOnError: false, // add errors to webpack instead of warnings
+      cwd: process.cwd() // set the current working directory for displaying module paths
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|ts)?$/,
+        include: [
+          path.resolve(__dirname, 'src'),
+          path.resolve('../../node_modules/@kogito-apps/common/src')
+        ],
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: path.resolve('./tsconfig.json'),
+              allowTsInNodeModules: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(svg|ttf|eot|woff|woff2)$/,
+        include: [
+          path.resolve('../../node_modules/patternfly/dist/fonts'),
+          path.resolve(
+            '../../node_modules/@patternfly/react-core/dist/styles/assets/fonts'
+          ),
+          path.resolve(
+            '../../node_modules/@patternfly/react-core/dist/styles/assets/pficon'
+          ),
+          path.resolve(
+            '../../node_modules/@patternfly/patternfly/assets/fonts'
+          ),
+          path.resolve(
+            '../../node_modules/@patternfly/patternfly/assets/pficon'
+          ),
+          path.resolve('./src/static'),
+          path.resolve('../../node_modules/@kogito-apps/common/src/static')
+        ],
+        use: {
+          loader: 'file-loader',
+          options: {
+            // Limit at 50k. larger files emited into separate files
+            limit: 5000,
+            outputPath: 'fonts',
+            name: '[name].[ext]'
+          }
+        }
+      },
+      {
+        test: /\.svg$/,
+        include: input => input.indexOf('background-filter.svg') > 1,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 5000,
+              outputPath: 'svgs',
+              name: '[name].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.svg$/,
+        include: input => input.indexOf(BG_IMAGES_DIRNAME) > -1,
+        use: {
+          loader: 'svg-url-loader',
+          options: {}
+        }
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif)$/i,
+        include: [
+          path.resolve(__dirname, 'src'),
+          path.resolve('../../node_modules/patternfly'),
+          path.resolve(
+            '../../node_modules/@patternfly/patternfly/assets/images'
+          ),
+          path.resolve(
+            '../../node_modules/@patternfly/react-styles/css/assets/images'
+          ),
+          path.resolve(
+            '../../node_modules/@patternfly/react-core/dist/styles/assets/images'
+          ),
+          path.resolve(
+            '../../node_modules/@patternfly/react-core/node_modules/@patternfly/react-styles/css/assets/images'
+          ),
+          path.resolve(
+            '../../node_modules/@patternfly/react-table/node_modules/@patternfly/react-styles/css/assets/images'
+          ),
+          path.resolve('../../node_modules/@kogito-apps/common/src/static')
+        ],
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 5000,
+              outputPath: 'images',
+              name: '[name].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    modules: [
+      path.resolve('../../node_modules'),
+      path.resolve('./node_modules'),
+      path.resolve('./src')
+    ],
+    plugins: [
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, './tsconfig.json')
+      })
+    ],
+    symlinks: false,
+    cacheWithContext: false
+  }
+};
